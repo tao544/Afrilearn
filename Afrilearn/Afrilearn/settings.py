cdfd9692b7b10506bea5f0ef73b92cbac767eb28
tao544 <taoheedadepoju1@gmail.com>
2026-01-05 12:09:02 +0100

    render corrections

--- Afrilearn/Afrilearn/settings.py
+++ Afrilearn/Afrilearn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3n@)ncqo0_#rln6@5c7o5nwd2&()8v$8nyd=^l-77^r23kovb-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.whitenoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =[
     BASE_DIR / "static"
 ]
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
